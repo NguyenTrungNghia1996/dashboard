a4988b2e7b00fc394e145de474fc9029a64e8f7c
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2025-04-01 15:35:57 +0700

    test

--- app.vue
+++ app.vue
@@ -2,5 +2,9 @@
   <div>
     <p class="bg-red-600">123123</p>
     <a-button type="primary">Primary Button</a-button>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button type="primary">Primary Button</a-button>
   </div>
 </template>
